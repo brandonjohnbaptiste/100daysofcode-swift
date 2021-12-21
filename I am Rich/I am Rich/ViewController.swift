1324f6636e77f451478185294705b1cce6ec8f31
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2021-12-21 14:54:45 +0000

    day 1 initial commit

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Brandon John-Baptiste on 21/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
