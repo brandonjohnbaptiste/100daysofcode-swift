d634e48846d1868a2fa82c8c5027e09e1d6af9f0
Brandon John-Baptiste <brandonjohnbaptiste@gmail.com>
2021-12-27 00:36:40 +0000

    I am broke app initial commit

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Brandon John-Baptiste on 27/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
